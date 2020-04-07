fdf0c2957401c3d2b1ac18dc48342223ef4df376
benchislett <chislett.ben@gmail.com>
2020-04-07 13:06:59 -0230

    Add Slab (AABB) and methods for bvh

--- src/primitive.cuh
+++ src/primitive.cuh
@@ -2,6 +2,21 @@
 
 #include "math.h"
 
+struct Slab {
+  Vec3 ll;
+  Vec3 ur;
+
+  Slab() {
+    ll = { FLT_MAX, FLT_MAX, FLT_MAX };
+    ur = { -FLT_MAX, -FLT_MAX, -FLT_MAX };
+  }
+
+  void expand(const Slab &s) {
+    ll = min(ll, s.ll);
+    ur = max(ur, s.ur);
+  }
+}
+
 struct Tri {
   Vec3 a;
   Vec3 b;
@@ -9,9 +24,5 @@ struct Tri {
   Vec3 n_a;
   Vec3 n_b;
   Vec3 n_c;
-}
-
-struct Slab {
-  Vec3 ll;
-  Vec3 ur;
+  Slab bound;
 }
