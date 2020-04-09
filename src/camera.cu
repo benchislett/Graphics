b4a1b2f21eb1df3daa7fe3e78677af1c42bd0808
benchislett <chislett.ben@gmail.com>
2020-04-09 12:34:13 -0230

    Rename variables in Camera::Camera for clarity

--- src/camera.cu
+++ src/camera.cu
@@ -1,22 +1,24 @@
 #include "camera.cuh"
 
+#include <cstdio>
+
 Camera::Camera(float vfov, float aspect, const Vec3 &look_from, const Vec3 &look_at, const Vec3 &view_up) {
   pos = look_from;
-  Vec3 u, v, w;
+  Vec3 u_, v_, w_;
 
-  float half_height = tanf(vfov / 2); // vfov should be in radians
+  float half_height = tanf(vfov / 2.f); // vfov should be in radians
   float half_width = half_height * aspect;
 
-  w = normalized(look_from - look_at);
-  u = normalized(cross(view_up, w));
-  v = cross(w, u);
+  w_ = normalized(look_from - look_at);
+  u_ = normalized(cross(view_up, w_));
+  v_ = cross(w_, u_);
 
-  lower_left = look_from - (half_width * u) - (half_height * v) - w;
+  lower_left = look_from - (half_width * u_) - (half_height * v_) - w_;
 
-  h = 2 * half_width * u;
-  v = 2 * half_height * v;
+  h = 2 * half_width * u_;
+  v = 2 * half_height * v_;
 }
 
 Ray Camera::get_ray(float s, float t) const {
-  return { pos, lower_left + (s * h) + (t * v) - pos };
+  return { pos, normalized(lower_left + (s * h) + (t * v) - pos) };
 }
