bd6025f26981432e43cfbbd9fcae18a1be71f1a2
benchislett <chislett.ben@gmail.com>
2020-04-08 23:29:44 -0230

    Refactor Ray

---
+++ src/ray.cu
@@ -0,0 +1,4 @@
+#include "ray.cuh"
+
+Vec3 Ray::at(float t) const { return Vec3(o.e[0] + t * d.e[0], o.e[1] + t * d.e[1], o.e[2] + t * d.e[2]); }
+
--- src/ray.cuh
+++ src/ray.cuh
@@ -6,5 +6,5 @@ struct Ray {
   Vec3 o;
   Vec3 d;
 
-  Vec3 at(float t) const { return Vec3(o.e[0] + t * d.e[0], o.e[1] + t * d.e[1], o.e[2] + t * d.e[2]); }
-}
+  Vec3 at(float t) const;
+};
