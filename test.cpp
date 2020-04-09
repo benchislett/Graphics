07ebcfc7ec58bead7e9c4906483b6a80d5b25ea4
benchislett <chislett.ben@gmail.com>
2020-04-09 12:37:35 -0230

    Reduce fov in test.cpp

--- test.cpp
+++ test.cpp
@@ -8,17 +8,17 @@ int main() {
   int height = 64;
   int spp = 1;
 
-  float vfov = 1.5708; // 90 degrees
+  float vfov = 0.698132;
   float aspect = 1; // square
 
-  Vec3 look_from = {3.f, 3.f, 3.f};
+  Vec3 look_from = {1.f, 1.f, 1.f};
   Vec3 look_at = {0.f, 0.f, 0.f};
   Vec3 view_up = {0.f, 1.f, 0.f};
 
   Camera c(vfov, aspect, look_from, look_at, view_up);
 
   Vec3 background = {0.2f, 0.2f, 0.75f};
-  
+
   Scene s = {c, NULL, 0, background, spp};
   load_tris_obj("data/bunny.obj", &s);
 
