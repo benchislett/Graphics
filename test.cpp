4669120fc9529610fca99b6745318c2fe282d498
benchislett <chislett.ben@gmail.com>
2020-04-18 13:09:49 -0230

    Update test.cpp for new io

--- test.cpp
+++ test.cpp
@@ -5,35 +5,28 @@
 
 int main() {
 
-  int width = 32;
-  int height = 32;
-  int spp = 1024;
+  int width = 128;
+  int height = 128;
+  int spp = 2048;
 
   float vfov = 0.698132;
   float aspect = 1; // square
 
-  Vec3 look_from = {1.f, 1.f, 1.f};
-  Vec3 look_at = {0.f, 0.f, 0.f};
+  Vec3 look_from = {0.f, 1.f, 4.f};
+  Vec3 look_at = {0.f, 1.f, -1.f};
   Vec3 view_up = {0.f, 1.f, 0.f};
 
-  Camera c(vfov, aspect, look_from, look_at, view_up);
-
-  Vec3 background = {0.2f, 0.2f, 0.75f};
-
-  int n_prims;
-  Primitive *prims = load_obj("data/bunny.obj", &n_prims);
-
-  BVH b = build_bvh(prims, n_prims);
+  Scene s = load_obj("data/cornell.obj");
+  s.cam = Camera(vfov, aspect, look_from, look_at, view_up);
 
   RenderParams params = {spp};
-  Scene s = {c, b, background};
 
   Image im = {width, height, NULL};
   im.film = (Vec3 *)malloc(width * height * sizeof(Vec3));
 
   Render(params, s, im);
   
-  write_ppm("bunny.ppm", im);
+  write_ppm("CornellBox.ppm", im);
   
   return 0;
 }
