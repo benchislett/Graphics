4abdb24a01d2582bf098fbbbdfb8c443f87f24c4
benchislett <chislett.ben@gmail.com>
2020-04-18 13:07:30 -0230

    Adjust bounding_slab computation to avoid 0-widths

--- src/shape.cu
+++ src/shape.cu
@@ -11,21 +11,21 @@ void Slab::expand(const Slab &s) {
 }
 
 Slab bounding_slab(const Slab &s) {
-  return { s.ll - 0.001, s.ur + 0.001 };
+  return { s.ll - 0.001f, s.ur + 0.001f };
 }
 
 Slab bounding_slab(const Slab &s1, const Slab &s2) {
-  return { { fmin(s1.ll.e[0], s2.ll.e[0]), fmin(s1.ll.e[1], s2.ll.e[1]), fmin(s1.ll.e[2], s2.ll.e[2])},
-           { fmax(s1.ur.e[0], s2.ur.e[0]), fmax(s1.ur.e[1], s2.ur.e[1]), fmax(s1.ur.e[2], s2.ur.e[2])}};
+  return { { fmin(s1.ll.e[0], s2.ll.e[0]) - 0.0001f, fmin(s1.ll.e[1], s2.ll.e[1]) - 0.0001f, fmin(s1.ll.e[2], s2.ll.e[2]) - 0.0001f},
+           { fmax(s1.ur.e[0], s2.ur.e[0]) + 0.0001f, fmax(s1.ur.e[1], s2.ur.e[1]) + 0.0001f, fmax(s1.ur.e[2], s2.ur.e[2]) + 0.0001f}};
 }
 
 Slab bounding_slab(const Vec3 &a, const Vec3 &b, const Vec3 &c) {
-  return { { fmin(fmin(a.e[0], b.e[0]), c.e[0]),
-             fmin(fmin(a.e[1], b.e[1]), c.e[1]),
-             fmin(fmin(a.e[2], b.e[2]), c.e[2]) },
-           { fmax(fmax(a.e[0], b.e[0]), c.e[0]),
-             fmax(fmax(a.e[1], b.e[1]), c.e[1]),
-             fmax(fmax(a.e[2], b.e[2]), c.e[2]) }};
+  return { { fmin(fmin(a.e[0], b.e[0]), c.e[0]) - 0.0001f,
+             fmin(fmin(a.e[1], b.e[1]), c.e[1]) - 0.0001f,
+             fmin(fmin(a.e[2], b.e[2]), c.e[2]) - 0.0001f },
+           { fmax(fmax(a.e[0], b.e[0]), c.e[0]) + 0.0001f,
+             fmax(fmax(a.e[1], b.e[1]), c.e[1]) + 0.0001f,
+             fmax(fmax(a.e[2], b.e[2]), c.e[2]) + 0.0001f }};
 }
 
 Tri::Tri(const Vec3 &a, const Vec3 &b, const Vec3 &c) : a(a), b(b), c(c), n_a(cross(b - a, c - a)), n_b(n_a), n_c(n_a), bound(bounding_slab(a, b, c)) {}
