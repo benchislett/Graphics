befbf1fe954967bba3100e30a19af3e328175e44
benchislett <chislett.ben@gmail.com>
2020-04-28 15:48:56 -0230

    Add Texture structure

---
+++ src/texture.cu
@@ -0,0 +1,19 @@
+#include "texture.cuh"
+
+#include "lodepng/lodepng.h"
+
+Texture::Texture(const std::string &png_name) {
+  std::vector<unsigned char> pixels;
+  unsigned error = lodepng::decode(pixels, width, height, png_name.c_str());
+
+  if (error) {
+    fprintf(stderr, "Error loading texture from %s\n", png_name.c_str());
+    data = NULL;
+    return;
+  }
+
+  data = (Vec3 *)malloc(width * height * sizeof(Vec3));
+  for (int i = 0; i < pixels.size(); i += 4) {
+    data[i / 4] = {(float)pixels[i] / 255.f, (float)pixels[i + 1] / 255.f, (float)pixels[i + 2] / 255.f};
+  }
+}
---
+++ src/texture.cuh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "math.cuh"
+
+#include <string>
+
+struct Texture {
+  Vec3 *data; // row-major
+  uint32_t width;
+  uint32_t height;
+
+  Texture() : data(NULL), width(0), height(0) {}
+  Texture(const std::string &png_name);
+};
