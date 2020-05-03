9c150e0f71bbdb7a23c6222fc2b43ee37cae8281
benchislett <chislett.ben@gmail.com>
2020-05-03 13:47:01 -0230

    Add movement between device and host for vector

--- src/vector.cuh
+++ src/vector.cuh
@@ -1,14 +1,36 @@
 #pragma once
 
+#include "cuda.cuh"
+
 template<typename T>
 struct Vector {
   T *data;
   int n;
+  bool is_host;
 
-  Vector() : data(NULL), n(0) {}
-  Vector(int n) : n(n) { data = (T *)calloc(n, sizeof(T)); }
-  Vector(T *data, int n) : data(data), n(n) {}
+  Vector() : data(NULL), n(0), is_host(true) {}
+  Vector(int n) : n(n), is_host(true) { data = (T *)calloc(n, sizeof(T)); }
+  Vector(T *data, int n) : data(data), n(n), is_host(true) {}
 
   T& operator[](int i) const { return data[i]; }
   int size() const { return n; }
+
+  void to_device() {
+    if (!is_host) return;
+    T *device_data;
+    cudaMalloc((void **)&device_data, n * sizeof(T));
+    cudaMemcpy(device_data, data, n * sizeof(T), cudaMemcpyHostToDevice);
+    free(data);
+    data = device_data;
+    is_host = false;
+  }
+
+  void to_host() {
+    if (is_host) return;
+    T *host_data = (T *)malloc(n * sizeof(T));
+    cudaMemcpy(host_data, data, n * sizeof(T), cudaMemcpyDeviceToHost);
+    cudaFree(data);
+    data = host_data;
+    is_host = true;
+  }
 };
