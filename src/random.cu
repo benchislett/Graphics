c52f44606f05d765d0b6abec20e6931db52ec795
benchislett <chislett.ben@gmail.com>
2020-05-03 14:04:32 -0230

    Add device-side rng

--- src/random.cu
+++ src/random.cu
@@ -11,3 +11,30 @@ float RNG::generate_float(float low, float high) const {
 
   return distribution(*twister);
 }
+
+__global__ void random_init(int xmax, curandState *state) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  if (i >= xmax) return;
+
+  curand_init(1984, i, 0, &state[i]);
+}
+
+DeviceRNG::DeviceRNG(int n_threads) {
+  cudaMalloc((void **)&state, n_threads * sizeof(curandState));
+  random_init<<<n_threads / 64 + 1, 64>>>(n_threads, state);
+  cudaDeviceSynchronize();
+}
+
+__device__ float LocalDeviceRNG::generate() const {
+  return curand_uniform(local_state);
+}
+
+__device__ int LocalDeviceRNG::generate_int(int low, int high) const {
+  float r = 1.f - generate();
+  return low + (int)(r * (high - low + 1));
+}
+
+__device__ float LocalDeviceRNG::generate_float(float low, float high) const {
+  float r = generate();
+  return low + (r * (high - low));
+}
--- src/random.cuh
+++ src/random.cuh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "cuda.cuh"
+
 #include <random>
 
 struct RNG {
@@ -10,4 +12,24 @@ struct RNG {
 
   int generate_int(int low, int high) const;
   float generate_float(float low = 0.f, float high = 1.f) const;
+};
+
+struct LocalDeviceRNG {
+  curandState *local_state;
+
+  LocalDeviceRNG(curandState *s) : local_state(s) {}
+
+  __device__ float generate() const;
+  __device__ int generate_int(int low, int high) const;
+  __device__ float generate_float(float low, float high) const;
+};
+
+struct DeviceRNG {
+  curandState *state;
+
+  DeviceRNG(int n_threads);
+
+  __device__ LocalDeviceRNG local(int tid) {
+    return LocalDeviceRNG(&state[tid]);
+  }
 };
