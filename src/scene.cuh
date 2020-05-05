5f32514e1cd4c192f57bffa41b4c4ade238a66b8
benchislett <chislett.ben@gmail.com>
2020-05-05 16:12:54 -0230

    Add to_host and to_device for Scene

--- src/scene.cuh
+++ src/scene.cuh
@@ -15,4 +15,22 @@ struct Scene {
   Vector<BSDF> materials;
   Vector<Texture> textures;
   DeviceRNG gen;
+
+  void to_host() {
+    b.to_host();
+    prims.to_host();
+    lights.to_host();
+    materials.to_host();
+    textures.to_host();
+    for (int i = 0; i < textures.size(); i++) textures[i].to_host();
+  }
+
+  void to_device() {
+    b.to_device();
+    prims.to_device();
+    lights.to_device();
+    materials.to_device();
+    for (int i = 0; i < textures.size(); i++) textures[i].to_device();
+    textures.to_device();
+  }
 };
