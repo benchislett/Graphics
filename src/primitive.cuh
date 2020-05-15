dc275e021fb1a4280143ce4e1ed376ff30b31829
benchislett <chislett.ben@gmail.com>
2020-05-15 20:04:58 -0230

    Add equality comparison for primitives

--- src/primitive.cuh
+++ src/primitive.cuh
@@ -10,4 +10,6 @@ struct Primitive {
 
   __host__ __device__ Primitive() {}
   Primitive(const Tri &t, const BSDF &b) : t(t), bsdf(b) {}
+
+  __host__ __device__ bool operator==(const Primitive &p) const { return p.t.a == t.a && p.t.b == t.b && p.t.c == t.c; }
 };
