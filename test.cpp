5a3f23cc9ef3360e890a9e01f1bead8756084f34
benchislett <chislett.ben@gmail.com>
2020-05-14 22:28:34 -0230

    Update test.cpp for cornell box

--- test.cpp
+++ test.cpp
@@ -2,32 +2,34 @@
 #include "src/io.cuh"
 #include "src/intersection.cuh"
 #include "src/bvh.cuh"
+#include "src/random.cuh"
 
 int main() {
+  int width = 1024;
+  int height = 1024;
+  int spp = 1;
 
-  int width = 128;
-  int height = 128;
-  int spp = 32;
-
-  float vfov = 0.698132f;
+  float vfov = 0.79f;
   float aspect = 1.f;
 
-  Vec3 look_from = {4.f, 3.f, 4.f};
-  Vec3 look_at = {0.f, 0.f, 0.f};
-  Vec3 view_up = {0.f, 1.f, 0.f};
+  //Vec3 look_from(5.03f, 0.91f, -2.20f);
+  //Vec3 look_at(-0.21f, 0.83f, -0.34f);
+  Vec3 look_from(0.f, 1.f, 3.5f);
+  Vec3 look_at(0.f, 1.f, -1.f);
+
+  Vec3 view_up(0.f, 1.f, 0.f);
 
   Scene s;
-  load_obj("data/car.obj", &s);
+  load_obj("data/cornell.obj", &s);
   s.cam = Camera(vfov, aspect, look_from, look_at, view_up);
 
   RenderParams params = {spp};
 
-  Image im = {width, height, NULL};
-  im.film = (Vec3 *)malloc(width * height * sizeof(Vec3));
+  Image im(width, height);
 
-  Render(params, s, im);
+  render(params, s, im);
   
-  write_ppm("car.ppm", im);
+  write_ppm("cornell.ppm", im);
   
   return 0;
 }
