611b4a180515cb0af173f1ebc8f1827a2c995014
benchislett <chislett.ben@gmail.com>
2021-01-22 22:57:29 -0500

    Main test file
    Primary consumer of libraries and functions to make sure things are
    working properly

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "cu_math.h"
+#include "geometry.h"
+
+#include <iostream>
+
+int main() {
+  Ray r;
+  r.origin    = make_float3(0.f);
+  r.direction = make_float3(1.f, 0.f, 0.f);
+
+  Triangle t;
+  t.v0 = make_float3(5.f, 2.f, 0.f);
+  t.v1 = make_float3(5.f, -1.f, 1.f);
+  t.v2 = make_float3(5.f, -1.f, -1.f);
+
+  TriangleHitRecord rec = hit(r, t);
+
+  std::cout << rec.hit << '\n';
+  std::cout << rec.time << '\n';
+
+  return 0;
+}
