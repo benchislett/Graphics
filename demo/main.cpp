ac14b615a112621774a16b0c3a30f9762faf1367
benchislett <chislett.ben@gmail.com>
2021-02-12 23:00:17 -0500

    Add fine-tuned pan mode and screenshot on close

--- demo/main.cpp
+++ demo/main.cpp
@@ -9,14 +9,15 @@
 #include <iostream>
 #include <string>
 
-constexpr int width    = 256;
-constexpr int height   = 256;
+constexpr int width    = 1024;
+constexpr int height   = 1024;
 constexpr float aspect = (float) width / (float) height;
 
 Scene scene;
 float3 position       = make_float3(0.f);
 float3 look_direction = make_float3(0.f, 0.f, -1.f);
 
+Image image;
 int main(int argc, char** argv) {
   scene = from_obj("data/cornell.obj");
 
@@ -26,27 +27,32 @@ int main(int argc, char** argv) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::KeyPressed) {
+        float pan_factor = 0.5f;
+        if (event.key.control) {
+          pan_factor /= 10.f;
+        }
         if (event.key.code == sf::Keyboard::W) {
-          position.z -= 0.5f;
+          position.z -= pan_factor;
         } else if (event.key.code == sf::Keyboard::S) {
-          position.z += 0.5f;
+          position.z += pan_factor;
         } else if (event.key.code == sf::Keyboard::A) {
-          position.x -= 0.5f;
+          position.x -= pan_factor;
         } else if (event.key.code == sf::Keyboard::D) {
-          position.x += 0.5f;
+          position.x += pan_factor;
         } else if (event.key.code == sf::Keyboard::Space) {
-          position.y += 0.5f;
+          position.y += pan_factor;
         } else if (event.key.code == sf::Keyboard::LShift) {
-          position.y -= 0.5f;
+          position.y -= pan_factor;
         }
       }
       if (event.type == sf::Event::Closed) {
         window.close();
+        to_ppm(image, "output/cornell.ppm");
       }
     }
 
     Camera camera = make_camera(position, look_direction + position, 1.57f, aspect);
-    Image image   = render(camera, scene, width, height, 1);
+    image         = render(camera, scene, width, height, 1);
 
     sf::Image image_data;
     image_data.create(width, height, (const sf::Uint8*) image.data);
