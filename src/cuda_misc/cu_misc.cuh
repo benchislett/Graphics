7e67c0b5b32ef832ef2ae41a26ede13b6b2ef99c
benchislett <chislett.ben@gmail.com>
2021-02-20 00:52:49 -0500

    C-style vectors for CUDA compliance

--- src/cuda_misc/cu_misc.cuh
+++ src/cuda_misc/cu_misc.cuh
@@ -2,6 +2,8 @@
 
 #include "cu_error.cuh"
 
+#include <vector>
+
 #define HD  __host__ __device__
 #define IHD inline HD
 
@@ -11,27 +13,60 @@ struct DeviceVector;
 template <typename T>
 struct HostVector {
   int size;
+  int capacity;
   T* data;
 
-  __host__ HostVector(int n) {
-    size = n;
-    data = (T*) malloc(n * sizeof(T));
-    assertNotNull(data);
+  __host__ HostVector() : size(0), capacity(0), data(NULL) {}
+  __host__ HostVector(int n) : size(0), capacity(0), data(NULL) {
+    resize(n);
+  }
+
+  __host__ void realloc(int n) {
+    if (capacity >= n) {
+      return;
+    }
+
+    capacity = n + 1;
+
+    T* new_data = (T*) malloc(capacity * sizeof(T));
+    assertNotNull(new_data);
+    if (data != NULL) {
+      memcpy(new_data, data, capacity * sizeof(T));
+      free(data);
+    }
+    data = new_data;
+  }
+
+  __host__ void resize(int n) {
+    realloc(n);
+    size = capacity;
   }
 
   __host__ T& operator[](const int i) {
     return data[i];
   }
 
+  __host__ void push_back(const T& item) {
+    size++;
+    if (size >= capacity) {
+      realloc(2 * size);
+    }
+    data[size - 1] = item;
+  }
+
   __host__ HostVector& operator=(const DeviceVector<T>& device_vector) {
-    cudaMemcpy(data, device_vector.data, size * sizeof(T), cudaMemcpyDeviceToHost);
+    resize(device_vector.size);
+    assertNotNull(data);
+    cudaMemcpy(data, device_vector.data, device_vector.size * sizeof(T), cudaMemcpyDeviceToHost);
     printf("Copy device to host\n");
     cudaCheckError();
     return *this;
   }
 
-  __host__ ~HostVector() {
-    free(data);
+  __host__ void destroy() {
+    if (data != NULL) {
+      free(data);
+    }
   }
 };
 
@@ -40,10 +75,37 @@ struct DeviceVector {
   int size;
   T* data;
 
+  __host__ DeviceVector() {
+    size = 0;
+    data = NULL;
+  }
+
   __host__ DeviceVector(int n) {
+    size = 0;
+    data = NULL;
+    resize(n);
+  }
+
+  __host__ void resize(int n) {
+    if (size >= n) {
+      if (n == 0) {
+        cudaFree(data);
+        cudaCheckError();
+        data = NULL;
+      }
+    } else {
+      T* new_data;
+      cudaMalloc((void**) &new_data, n * sizeof(T));
+      cudaCheckError();
+      if (data != NULL) {
+        cudaMemcpy(new_data, data, size * sizeof(T), cudaMemcpyDeviceToDevice);
+        cudaCheckError();
+        cudaFree(data);
+        cudaCheckError();
+      }
+      data = new_data;
+    }
     size = n;
-    cudaMalloc((void**) &data, n * sizeof(T));
-    cudaCheckError();
   }
 
   __device__ T& operator[](const int i) {
@@ -51,13 +113,17 @@ struct DeviceVector {
   }
 
   __host__ DeviceVector& operator=(const HostVector<T>& host_vector) {
-    cudaMemcpy(data, host_vector.data, size * sizeof(T), cudaMemcpyHostToDevice);
+    resize(host_vector.size);
+    assertNotNull(data);
+    cudaMemcpy(data, host_vector.data, host_vector.size * sizeof(T), cudaMemcpyHostToDevice);
     cudaCheckError();
     return *this;
   }
 
-  __host__ ~DeviceVector() {
-    // cudaFree(data);
-    cudaCheckError();
+  __host__ void destroy() {
+    if (data != NULL) {
+      cudaFree(data);
+      cudaCheckError();
+    }
   }
 };
