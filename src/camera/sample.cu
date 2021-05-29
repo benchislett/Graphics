df21625a3ff1c970ab310d07ba1d743bdb37cff9
benchislett <chislett.ben@gmail.com>
2021-05-29 17:54:21 -0400

    Clean slate
    ...again

--- src/camera/sample.cu
+++
@@ -1,9 +0,0 @@
-#include "camera.cuh"
-
-HD Ray get_ray(const Camera camera, float u, float v) {
-  v                = 1.f - v;
-  float3 x         = u * camera.horizontal;
-  float3 y         = v * camera.vertical;
-  float3 direction = camera.project_lower_left - camera.position;
-  return (Ray){camera.position, normalized(direction + x + y)};
-}
--- src/integrator/integrate.cuh
+++
@@ -1,6 +0,0 @@
-#include "camera.cuh"
-#include "cu_math.cuh"
-#include "image.cuh"
-#include "scene.cuh"
-
-Image render(const Camera camera, DeviceScene& scene, int x, int y, int spp);
