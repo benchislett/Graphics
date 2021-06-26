0aa6239481c5735199ee9d17d27a42bb3b12cb00
benchislett <chislett.ben@gmail.com>
2021-06-25 21:47:04 -0400

    Implement simple tests for Ray-AABB

---
+++ test/geometry/aabb.cpp
@@ -0,0 +1,32 @@
+#define CATCH_CONFIG_MAIN
+#include "aabb.cuh"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Easterly Ray hits Eastern AABB", "[AABB.intersects(Ray)]") {
+  Ray r({0, 0, 0}, {1, 0, 0});
+  AABB b({1, -1, -1}, {3, 1, 1}); // 2x2x2 about (2, 0, 0)
+
+  REQUIRE(b.intersects(r));
+}
+
+TEST_CASE("Easterly Ray misses Western AABB", "[AABB.intersects(Ray)]") {
+  Ray r({0, 0, 0}, {1, 0, 0});
+  AABB b({-3, -1, -1}, {-1, 1, 1}); // 2x2x2 about (-2, 0, 0)
+
+  REQUIRE_FALSE(b.intersects(r));
+}
+
+TEST_CASE("Westerly Ray hits Western AABB", "[AABB.intersects(Ray)]") {
+  Ray r({0, 0, 0}, {-1, 0, 0});
+  AABB b({-3, -1, -1}, {-1, 1, 1}); // 2x2x2 about (-2, 0, 0)
+
+  REQUIRE(b.intersects(r));
+}
+
+TEST_CASE("Westerly Ray misses Eastern AABB", "[AABB.intersects(Ray)]") {
+  Ray r({0, 0, 0}, {-1, 0, 0});
+  AABB b({1, -1, -1}, {3, 1, 1}); // 2x2x2 about (2, 0, 0)
+
+  REQUIRE_FALSE(b.intersects(r));
+}
