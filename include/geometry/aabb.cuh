5b6cc170427d790b9a7650b17a0335b1cf1d22d4
benchislett <chislett.ben@gmail.com>
2021-06-25 21:46:37 -0400

    Geometry: AABB struct and Ray-AABB intersector

---
+++ include/geometry/aabb.cuh
@@ -0,0 +1,11 @@
+#pragma once
+#include "float3.cuh"
+#include "primitive.cuh"
+
+struct AABB : Primitive {
+  float3 lo;
+  float3 hi;
+  AABB(float3 l, float3 h) : lo(l), hi(h) {}
+
+  bool intersects(Ray r) const;
+};
---
+++ src/geometry/aabb.cu
@@ -0,0 +1,17 @@
+#include "aabb.cuh"
+
+bool AABB::intersects(Ray r) const {
+  double tx1 = (lo.x - r.o.x) / r.d.x;
+  double tx2 = (hi.x - r.o.x) / r.d.x;
+
+  double tmin = min(tx1, tx2);
+  double tmax = max(tx1, tx2);
+
+  double ty1 = (lo.y - r.o.y) / r.d.y;
+  double ty2 = (hi.y - r.o.y) / r.d.y;
+
+  tmin = max(tmin, min(ty1, ty2));
+  tmax = min(tmax, max(ty1, ty2));
+
+  return tmax >= tmin && tmax >= 0.f;
+}
