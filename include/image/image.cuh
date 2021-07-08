6724be495baa09724a8568a892ec1b5b8ffeeffc
benchislett <chislett.ben@gmail.com>
2021-07-08 18:11:06 -0400

    Add Image.to_png and tests

--- include/image/image.cuh
+++ include/image/image.cuh
@@ -11,9 +11,15 @@ struct Image {
   unsigned int height;
 
   Image() : width(0), height(0), data(nullptr) {}
+  Image(unsigned w, unsigned h) : width(w), height(h), data((float3*) malloc(w * h * sizeof(float3))) {}
   Image(unsigned int w, unsigned int h, float3* d) : data(d), width(w), height(h) {}
   Image(unsigned int w, unsigned int h, float* d) : data((float3*) d), width(w), height(h) {}
   Image(const std::string& filename);
 
+  float3& operator[](int i) {
+    return data[i];
+  }
+
   void to_png(const std::string& filename) const;
+  void destroy();
 };
--- src/image/image.cu
+++ src/image/image.cu
@@ -1,20 +1,47 @@
 #include "image.cuh"
 #include "lodepng.h"
 
+#include <iostream>
+#include <vector>
+
 Image::Image(const std::string& filename) {
-  unsigned char* pixels = nullptr;
+  std::vector<unsigned char> pixels;
 
-  lodepng_decode32_file(&pixels, &width, &height, filename.c_str());
+  unsigned error = lodepng::decode(pixels, width, height, filename);
+  if (error) {
+    std::cerr << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+  }
 
-  float3* data = (float3*) malloc(width * height * sizeof(float3));
+
+  data = (float3*) malloc(width * height * sizeof(float3));
   for (unsigned int i = 0; i < width * height * 4; i += 4) {
-    float r = (float) pixels[i + 0] / 255.0;
-    float g = (float) pixels[i + 1] / 255.0;
-    float b = (float) pixels[i + 2] / 255.0;
+    float r = ((float) pixels[i + 0]) / 255.0;
+    float g = ((float) pixels[i + 1]) / 255.0;
+    float b = ((float) pixels[i + 2]) / 255.0;
     // ignore alpha
 
     data[i / 4] = {r, g, b};
   }
+}
 
-  free(pixels);
+void Image::destroy() {
+  free(data);
+  data = nullptr;
+}
+
+void Image::to_png(const std::string& filename) const {
+  std::vector<unsigned char> pixels(width * height * 4);
+
+  for (unsigned int i = 0; i < width * height * 4; i += 4) {
+    float3 rgb    = data[i / 4];
+    pixels[i + 0] = (unsigned char) (255.0 * rgb.x);
+    pixels[i + 1] = (unsigned char) (255.0 * rgb.y);
+    pixels[i + 2] = (unsigned char) (255.0 * rgb.z);
+    pixels[i + 3] = 255;
+  }
+
+  unsigned error = lodepng::encode(filename, pixels, width, height);
+  if (error) {
+    std::cerr << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+  }
 }
