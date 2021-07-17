9f74415399b788916e884f3306c54608a01c256d
benchislett <chislett.ben@gmail.com>
2021-07-16 20:35:18 -0400

    Link blender addon to core engine

--- src/pybind/bindings.cpp
+++ src/pybind/bindings.cpp
@@ -1,12 +1,23 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 namespace py = pybind11;
 #include "render.cuh"
 
+
 PYBIND11_MODULE(benptpy_core, m) {
+
   m.def(
       "render",
-      []() {
-        return (float3){1, 1, 1};
+      [](int width, int height) {
+        std::vector<std::array<float, 4>> out(width * height);
+        Triangle t({2, -1, -1}, {2, 1, -1}, {2, 0, 1});
+        Camera cam(M_PI / 4.0, 1.0, {-1, 0, 0}, {1, 0, 0});
+        Image img = render_normals(t, cam, width, height);
+        for (int i = 0; i < width * height; i++) {
+          out[i] = {img[i].x, img[i].y, img[i].z, 1.0};
+        }
+        img.destroy();
+        return out;
       },
       "Render");
 }
