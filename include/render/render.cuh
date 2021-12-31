c31cf1284fdb8723d441cbf00e77aef714bf5cd3
benchislett <chislett.ben@gmail.com>
2021-12-31 16:55:41 -0500

    Fix render_normals and hardcode dodecahedron

--- include/render/render.cuh
+++ include/render/render.cuh
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "../camera/camera.cuh"
-#include "../geometry/trimesh.cuh"
+#include "../geometry/tri_array.cuh"
+#include "../geometry/triangle.cuh"
 #include "../image/image.cuh"
 
-Image render_normals(Triangle tri, Camera cam, unsigned int w, unsigned int h);
+Image render_normals(TriangleArray tris, Vector<TriangleNormals> normals_arr, Camera cam, unsigned int w,
+                     unsigned int h);
--- src/render/render.cu
+++ src/render/render.cu
@@ -1,11 +1,15 @@
 #include "camera.cuh"
 #include "image.cuh"
 #include "render.cuh"
+#include "scoped_timer.cuh"
 #include "sphere.cuh"
-#include "trimesh.cuh"
+#include "tri_array.cuh"
 
+#include <chrono>
 #include <cuda.h>
+#include <functional>
 #include <iostream>
+#include <thread>
 
 #define cudaCheckError()                                                               \
   {                                                                                    \
@@ -16,15 +20,16 @@
     }                                                                                  \
   }
 
-__global__ void render_kernel_normals(Triangle t, Camera cam, float3* out, unsigned int w, unsigned int h) {
+__global__ void render_kernel_normals(TriangleArray tris, Vector<TriangleNormals> normals_arr, Camera cam, float3* out,
+                                      unsigned int w, unsigned int h) {
   int x = (blockIdx.x * blockDim.x) + threadIdx.x;
   int y = (blockIdx.y * blockDim.y) + threadIdx.y;
 
   if (x >= w || y >= h)
     return;
 
-  int spp_x  = 20;
-  int spp_y  = 20;
+  int spp_x  = 1;
+  int spp_y  = 1;
   int spp    = spp_x * spp_y;
   float3 rgb = {0, 0, 0};
 
@@ -35,28 +40,31 @@ __global__ void render_kernel_normals(Triangle t, Camera cam, float3* out, unsig
 
       Ray r = cam.get_ray(u, v);
 
-      auto i = t.intersects(r);
+      auto i = tris.intersects(r);
 
-      auto normals = TriangleNormals(t);
-      rgb += i.uvw;
-      // Vec3 normal  = normals.at(i.uvw, r);
-      // if (i.hit) {
-      //   rgb.x += (normal.x + 1.0) / 2.0;
-      //   rgb.y += (normal.y + 1.0) / 2.0;
-      //   rgb.z += (normal.z + 1.0) / 2.0;
-      // }
+      if (i.hit) {
+        auto normals = normals_arr[i.idx];
+
+        Vec3 normal = normals.at(i.uvw, r);
+        rgb.x += (normal.x + 1.0) / 2.0;
+        rgb.y += (normal.y + 1.0) / 2.0;
+        rgb.z += (normal.z + 1.0) / 2.0;
+      }
     }
   }
 
   out[y * w + x] = rgb / (float) spp;
 }
 
-Image render_normals(Triangle tri, Camera cam, unsigned int w, unsigned int h) {
+Image render_normals(TriangleArray tris, Vector<TriangleNormals> normals_arr, Camera cam, unsigned int w,
+                     unsigned int h) {
   Image out(w, h);
+
+  ScopedMicroTimer x([&](int us) { printf("Rendered in %.2f ms\n", (double) us / 1000.0); });
 
   dim3 block(16, 16);
   dim3 grid((w + 15) / 16, (h + 15) / 16);
-  render_kernel_normals<<<grid, block>>>(tri, cam, out.values.data, w, h);
+  render_kernel_normals<<<grid, block>>>(tris, normals_arr, cam, out.values.data, w, h);
   cudaDeviceSynchronize();
   cudaCheckError();
 
