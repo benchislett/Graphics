b94e3086e124ec1a8b167d44c38e08cf3035d671
benchislett <chislett.ben@gmail.com>
2022-01-06 18:20:23 -0500

    Naive wavefront rendering

--- src/render/render.cu
+++ src/render/render.cu
@@ -20,53 +20,91 @@
     }                                                                                  \
   }
 
-__global__ void render_kernel_normals(TriangleArray tris, Vector<TriangleNormals> normals_arr, Camera cam, float3* out,
-                                      unsigned int w, unsigned int h) {
-  int x = (blockIdx.x * blockDim.x) + threadIdx.x;
-  int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+struct Path {
+  Ray cur;
+  float3 L;
+  int px;
+  int py;
+  bool active;
+};
 
-  if (x >= w || y >= h)
+__global__ void init_paths(Vector<Path> pq, Camera cam, unsigned int w, unsigned int h, unsigned int spp,
+                           unsigned int paths_processed) {
+  unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+  int pp = paths_processed + idx;
+
+  if (idx >= pq.size)
     return;
 
-  int spp_x  = 1;
-  int spp_y  = 1;
-  int spp    = spp_x * spp_y;
-  float3 rgb = {0, 0, 0};
+  int spp_rt = (int) sqrtf((float) spp);
 
-  for (int xx = 0; xx < spp_x; xx++) {
-    for (int yy = 0; yy < spp_y; yy++) {
-      float u = ((float) x + (float) xx / (float) spp_x) / (float) w;
-      float v = ((float) y + (float) yy / (float) spp_y) / (float) h;
+  int sample = pp / (w * h);
+  int coord  = pp % (w * h);
+  int x      = coord % w;
+  int y      = coord / w;
+  int sx     = sample % spp_rt;
+  int sy     = sample / spp_rt;
 
-      Ray r = cam.get_ray(u, v);
+  float u = ((float) x + (float) sx / (float) spp_rt) / (float) w;
+  float v = ((float) y + (float) sy / (float) spp_rt) / (float) h;
 
-      auto i = tris.intersects(r);
+  Ray r = cam.get_ray(u, v);
 
-      if (i.hit) {
-        auto normals = normals_arr[i.idx];
+  pq[idx] = {r, {0, 0, 0}, x, y, true};
+}
 
-        Vec3 normal = normals.at(i.uvw, r);
-        rgb.x += (normal.x + 1.0) / 2.0;
-        rgb.y += (normal.y + 1.0) / 2.0;
-        rgb.z += (normal.z + 1.0) / 2.0;
-      }
-    }
+__global__ void advance_paths(TriangleArray tris, Vector<TriangleNormals> normals_arr, Vector<Path> pq, Image out) {
+  unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+  if (idx >= pq.size)
+    return;
+
+  Path p = pq[idx];
+  Ray r  = p.cur;
+
+  auto i = tris.intersects(r);
+
+  if (i.hit) {
+    auto normals = normals_arr[i.idx];
+
+    Vec3 normal = normals.at(i.uvw, r);
+    p.L.x += (normal.x + 1.0) / 2.0;
+    p.L.y += (normal.y + 1.0) / 2.0;
+    p.L.z += (normal.z + 1.0) / 2.0;
   }
 
-  out[y * w + x] = rgb / (float) spp;
+  out[p.py * out.width + p.px] = p.L;
 }
 
 Image render_normals(TriangleArray tris, Vector<TriangleNormals> normals_arr, Camera cam, unsigned int w,
                      unsigned int h) {
   Image out(w, h);
 
-  ScopedMicroTimer x([&](int us) { printf("Rendered in %.2f ms\n", (double) us / 1000.0); });
 
-  dim3 block(16, 16);
-  dim3 grid((w + 15) / 16, (h + 15) / 16);
-  render_kernel_normals<<<grid, block>>>(tris, normals_arr, cam, out.values.data, w, h);
-  cudaDeviceSynchronize();
-  cudaCheckError();
+  unsigned int spp = 1;
+
+  unsigned int total_paths = w * h * spp;
+
+  unsigned int path_queue_size = min(total_paths, 1024 * 1024);
+  Vector<Path> path_queue(path_queue_size);
+
+  ScopedMicroTimer x_([&](int us) { printf("Rendered in %.2f ms\n", (double) us / 1000.0); });
+
+  unsigned int paths_processed = 0, rounds = 0;
+  while (paths_processed < total_paths) {
+    dim3 block(128);
+    dim3 grid((path_queue_size + 127) / 128);
+    init_paths<<<grid, block>>>(path_queue, cam, w, h, spp, paths_processed);
+    cudaDeviceSynchronize();
+    cudaCheckError();
+    advance_paths<<<grid, block>>>(tris, normals_arr, path_queue, out);
+    cudaDeviceSynchronize();
+    cudaCheckError();
+
+    rounds++;
+    paths_processed += path_queue_size;
+  }
 
   return out;
 }
