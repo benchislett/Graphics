a30cee9fbe46db6039675124f81feb7fd5a3c344
benchislett <chislett.ben@gmail.com>
2022-04-15 21:22:20 -0400

    Use unicode \approx for isapprox in tests

--- test/test_camera.jl
+++ test/test_camera.jl
@@ -10,24 +10,24 @@ using .GeometryTypes
     cam = PerspectiveCamera(1, π / 4, [1, 2.25, 4], [-0.5, -0.75, -1])
     ray = get_ray(cam, 0.3, 0.3)
 
-    @test isapprox(cam.horizontal, [0.793489, 0, -0.238047])
-    @test isapprox(cam.vertical, [-0.118612, 0.718263, -0.395374])
-    @test isapprox(cam.lower_left, [0.413425, 1.3926, 3.48626])
-    @test isapprox(cam.position, [1, 2.25, 4])
+    @test cam.horizontal ≈ [0.793489, 0, -0.238047]
+    @test cam.vertical ≈ [-0.118612, 0.718263, -0.395374]
+    @test cam.lower_left ≈ [0.413425, 1.3926, 3.48626]
+    @test cam.position ≈ [1, 2.25, 4]
 
-    @test isapprox(ray.origin, cam.position)
-    @test isapprox(ray.direction, [-0.373983, -0.624998, -0.685212])
+    @test ray.origin ≈ cam.position
+    @test ray.direction ≈ [-0.373983, -0.624998, -0.685212]
 
     cam = PerspectiveCamera(2, π / 2, [100, 150, 999], [29, 16, 32])
     ray = get_ray(cam, 0.8, 0.6)
 
-    @test isapprox(cam.horizontal, [3.98926, 0, -0.292903])
-    @test isapprox(cam.vertical, [-0.0200492, 1.98117, -0.273064])
-    @test isapprox(cam.lower_left, [97.9429, 148.873, 998.295])
-    @test isapprox(cam.position, [100, 150, 999])
+    @test cam.horizontal ≈ [3.98926, 0, -0.292903]
+    @test cam.vertical ≈ [-0.0200492, 1.98117, -0.273064]
+    @test cam.lower_left ≈ [97.9429, 148.873, 998.295]
+    @test cam.position ≈ [100, 150, 999]
 
-    @test isapprox(ray.origin, cam.position)
-    @test isapprox(ray.direction, [0.712626, 0.0388739, -0.700466])
+    @test ray.origin ≈ cam.position
+    @test ray.direction ≈ [0.712626, 0.0388739, -0.700466]
   end
 
   @testset "Orthographic Camera" begin
@@ -36,18 +36,18 @@ using .GeometryTypes
     ray_center = get_ray(cam, 0.5, 0.5)
     ray_opposite = get_ray(cam, 1, 1)
 
-    @test isapprox(cam.center, [10, 9, 11])
-    @test isapprox(cam.direction, [-1, 0, 0])
-    @test isapprox(cam.horizontal, [1, 0, 0])
-    @test isapprox(cam.vertical, [0, 1, 0])
+    @test cam.center ≈ [10, 9, 11]
+    @test cam.direction ≈ [-1, 0, 0]
+    @test cam.horizontal ≈ [1, 0, 0]
+    @test cam.vertical ≈ [0, 1, 0]
 
-    @test isapprox(ray_corner.origin, [9.5, 8.5, 11])
-    @test isapprox(ray_center.origin, [10, 9, 11])
-    @test isapprox(ray_opposite.origin, [10.5, 9.5, 11])
+    @test ray_corner.origin ≈ [9.5, 8.5, 11]
+    @test ray_center.origin ≈ [10, 9, 11]
+    @test ray_opposite.origin ≈ [10.5, 9.5, 11]
 
-    @test isapprox(ray_corner.direction, [-1, 0, 0])
-    @test isapprox(ray_center.direction, [-1, 0, 0])
-    @test isapprox(ray_opposite.direction, [-1, 0, 0])
+    @test ray_corner.direction ≈ [-1, 0, 0]
+    @test ray_center.direction ≈ [-1, 0, 0]
+    @test ray_opposite.direction ≈ [-1, 0, 0]
   end
 
 end
