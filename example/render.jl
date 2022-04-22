d070d9c881e4ecd842e3f6a8d90b6cba8eb02030
benchislett <chislett.ben@gmail.com>
2022-04-22 15:52:19 -0400

    Update render example scene options

--- example/render.jl
+++ example/render.jl
@@ -1,20 +1,32 @@
-include("../src/GraphicsCore.jl")
+using Plots, Images, ImageView
 
-using Plots
+using GraphicsCore.Renderer
+using GraphicsCore.OBJ
+using GraphicsCore.Cameras
+using GraphicsCore.GeometryTypes
+using GraphicsCore.SDFs
 
-using .GraphicsCore.Renderer
-using .GraphicsCore.OBJ
-using .GraphicsCore.Cameras
-using .GraphicsCore.GeometryTypes
-using .GraphicsCore.SDFs
+function getscene(sz=2048)
+  width = sz
+  height = sz
 
-# scene = DifferenceSDF(CubeSDF([0, 0, 0], 1), SphereSDF([0, 0, 0], 1.2))
+  # SDF sample scene
+  scene = SphereSDF([0, 0, 0], 1)
+  # scene = DifferenceSDF(CubeSDF([0, 0, 0], 1), SphereSDF([0, 0, 0], 1.25))
+  # scene = UnionSDF(scene, SphereSDF([0, 0, 0], 0.8))
+  scene = ModuloSDF(scene, [5, 5, 5])
+  cam = PerspectiveCamera(width / height, π / 4, [4, 2, 0], [0, 2, 0])
 
-function getscene()
-  width = 32
-  height = 32
-  scene = loadobjmesh("bunny.obj")
-  cam = PerspectiveCamera(width / height, π / 4, [2, 3, 2], [0, 1, 0])
+  # Mesh sample scene
+  # scene = loadobjmesh("bunny.obj")
+  # cam = PerspectiveCamera(width / height, π / 4, [2, 3, 2], [0, 1, 0])
 
   (scene, cam, width, height)
 end
+
+scene = getscene()
+
+@time img = render(scene...)
+# save("output.png", img)
+# imshow(img)
+plot(img)
