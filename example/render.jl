0aa04000cb9fd0bf63eecb6f20f03eb72732a7a2
benchislett <chislett.ben@gmail.com>
2022-04-24 12:50:36 -0400

    Animations and animated scenes

--- example/render.jl
+++ example/render.jl
@@ -6,7 +6,7 @@ using GraphicsCore.Cameras
 using GraphicsCore.GeometryTypes
 using GraphicsCore.SDFs
 
-function getscene(sz=64, t=0)
+function getscene(sz=32, t=0)
   width = sz
   height = sz
 
@@ -14,9 +14,10 @@ function getscene(sz=64, t=0)
   # scene = SphereSDF([0, 0, 0], 1)
   scene = DifferenceSDF(CubeSDF([0, 0, 0], 1), SphereSDF([0, 0, 0], 1.25))
   scene = UnionSDF(scene, SphereSDF([0, 0, 0], 0.8))
-  scene = IntersectSDF(scene, SphereSDF([0, 0, 0], 1.3))
+  scene = IntersectionSDF(scene, SphereSDF([0, 0, 0], 1.3))
   # scene = ModuloSDF(scene, [5, 5, 5])
-  cam = PerspectiveCamera(width / height, π / 4, [4 * cos(t), 4 - 2 * cos(t), 4 * sin(t)], [0, 0, 0])
+  # cam = PerspectiveCamera(width / height, π / 4, [4, 4, 4], [0, 0, 0])
+  cam = PerspectiveCamera(width / height, π / 4, 1.5 .* [2cos(3t), sin(2t)^2 - 2cos(t), 3sin(3t)], [0, 0, 0])
 
   # Mesh sample scene
   # scene = loadobjmesh("bunny.obj")
@@ -25,14 +26,20 @@ function getscene(sz=64, t=0)
   (scene, cam, width, height)
 end
 
-t = 0
-# anim = @animate for t = 0:0.1:2π
-scene = getscene(32, t)
-img = render(scene...)
-# save("output.png", img)
-# plot(img)
-# end
+# t = 0
+function makeanim(sz=1024, int=0:0.01:2π)
+  @animate for t = int
+    scene = getscene(sz, t)
+    img = render(scene...)
+    # save("output.png", img)
+    plot(img; dpi=600, framestyle=:none, background=RGB(0, 0, 0))
+  end
+end
 
-# gif(anim, fps=8)
-# imshow(img)
-# plot(img)
+function makeimage(sz=1024)
+  scene = getscene(sz)
+  render(scene...)
+end
+
+# img = makeimage(512)
+gif(makeanim(512, 0:0.01:2π), fps=30)
