34edadcef6a9fc54652b0da58f4341778d27d5f7
benchislett <chislett.ben@gmail.com>
2022-08-13 17:36:12 -0400

    obj loader tweaks

--- src/obj.jl
+++ src/obj.jl
@@ -7,15 +7,15 @@ using ..GeometryTypes
 export loadobjmesh
 
 function loadobjmesh(filename::String)
-  mesh::Mesh = load(filename)
+  mesh = load(filename)
 
   function facetotri(f)
-    pts = coordinates(mesh)[f]
+    pts = [coordinates(mesh)[f[1]], coordinates(mesh)[f[2]], coordinates(mesh)[f[3]]]
     Triangle(pts[1], pts[2], pts[3])
   end
 
   function facetonormals(f)
-    pts = normals(mesh)[f]
+    pts = [normals(mesh)[f[1]], normals(mesh)[f[2]], normals(mesh)[f[3]]]
     TriangleNormals(pts[1], pts[2], pts[3])
   end
 
